a6869af43e2a13e1d81223069123c3723db0f466
Kay Hood <kay.moth.fuzz@gmail.com>
2022-01-01 00:58:11 -0600

    whoops lol

--- framework/render/camera.go
+++ framework/render/camera.go
@@ -27,7 +27,7 @@ func (c *Camera) Look(position Vec3, orientation Quat) {
 }
 func (c *Camera) Look2D(position Vec2) {
 	eye := Vec3{float32(c.width) / 2, float32(c.height) / 2, -float32(c.z2D)}
-	eye.Sub(position.Vec3(0))
+	eye = eye.Sub(position.Vec3(0))
 	c.view = LookAtV(eye, eye.Add(Vec3{0, 0, 1}), Vec3{0, -1, 0})
 }
 func (c *Camera) Init2D(width, height int32) {
