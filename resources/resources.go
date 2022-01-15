f7379d923d8c9976377addee63a57d2950e1b735
Kay Hood <kay.moth.fuzz@gmail.com>
2022-01-15 04:26:09 -0600

    custom resource directory is now optional

--- resources/resources.go
+++ resources/resources.go
@@ -13,7 +13,10 @@ var BaseResources embed.FS
 
 //convenience function to check both Resources and BaseResources
 func ReadResource(path string) ([]byte, error) {
-	bytes, err := fs.ReadFile(Resources, "resources/"+path)
+	bytes, err := []byte(nil), fs.ErrNotExist
+	if Resources != nil {
+		bytes, err = fs.ReadFile(Resources, "resources/"+path)
+	}
 	if err != nil {
 		bytes, err = BaseResources.ReadFile("base_resources/" + path)
 	}
